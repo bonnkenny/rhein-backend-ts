8e092a1975316d5233dc461da9056c2a8cbcca26
Devin <jiajiyuan@shinetechsoftware.com>
2024-09-18 10:45:34 +0800

    feat: export api-swagger.json only when debugging

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ import { AppModule } from './app.module';
 import validationOptions from './utils/validation-options';
 import { AllConfigType } from './config/config.type';
 import { ResolvePromisesInterceptor } from './utils/serializer.interceptor';
+import { writeFileSync } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
@@ -44,8 +45,11 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('docs', app, document);
-
+  SwaggerModule.setup('api-docs', app, document);
+  // console.log('app env', configService.get('app.nodeEnv', { infer: true }));
+  if (configService.get('app.nodeEnv', { infer: true }) === 'development') {
+    writeFileSync('./api-swagger.json', JSON.stringify(document));
+  }
   await app.listen(configService.getOrThrow('app.port', { infer: true }));
 }
 void bootstrap();
